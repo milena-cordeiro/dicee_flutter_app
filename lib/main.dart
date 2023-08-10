42ae8b60c3aa20834fea2937ff119c3d9eaf0cad
Milena_Santos <mcs9823@gmail.com>
2023-08-10 23:39:25 +0000

    using the expanded widget to create flexible layouts - usando expanded widget com imagens e adicionando imagens de um jeito diferente menos verboso

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,6 +20,15 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+      ],
+    );
   }
 }
